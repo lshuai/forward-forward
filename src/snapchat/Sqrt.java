edcc11a4fbe11094afd83700bc7cd534a5a2df90
Li Shuai <lshuai@li-mac-pro.hsd1.wa.comcast.net>
2016-09-24 00:32:59 -0700

    correct sqrt

--- src/snapchat/Sqrt.java
+++ src/snapchat/Sqrt.java
@@ -12,12 +12,12 @@ public class Sqrt {
             long mid = l + (r - l) / 2;
             long sqrt = mid * mid;
             if (sqrt == x)
-                return (long)mid;
+                return (int)mid;
             if (sqrt < x) 
                 l = mid + 1;
             else
                 r = mid - 1;
         }
-        return (long)r;
+        return (int)r;
     }
 }
