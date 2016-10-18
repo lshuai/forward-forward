1dc0a1df40f19b95ba529a61539b1f2f42a6fea1
Li Shuai <lshuai@li-mac-pro.hsd1.wa.comcast.net>
2016-10-17 23:37:38 -0700

    symmetic tree

---
+++ src/snapchat/SymmetricTree.java
@@ -0,0 +1,40 @@
+package snapchat;
+import java.util.*;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return false;
+        
+        List<TreeNode> cur = new ArrayList<>();
+        cur.add(root);
+        List<TreeNode> next = new ArrayList<>();
+        while (!cur.isEmpty()) {
+            int l = 0;
+            int r = cur.size() - 1;
+            while (l <= r) {
+                TreeNode left = cur.get(l++);
+                TreeNode right = cur.get(r--);
+                if (!isLeftRight(left, right) || !isRightLeft(left, right))
+                    return false;
+            }
+            for (TreeNode n: cur) {
+                if (n.left != null)
+                    next.add(n.left);
+                if (n.right != null)
+                    next.add(n.right);
+            }
+            cur = next;
+            next = new ArrayList<>();
+        }
+        return true;
+    }
+    
+    private static boolean isLeftRight(TreeNode a, TreeNode b) {
+        return a.left != null && b.right != null && a.left.val == b.right.val || (a.left == null && b.right == null);
+    }
+    
+    private static boolean isRightLeft(TreeNode a, TreeNode b) {
+        return a.right != null && b.left != null && a.right.val == b.left.val || (a.right == null && b.left == null);
+    }
+}
